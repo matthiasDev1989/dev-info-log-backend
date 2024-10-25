aca33fb5401d317ad1c127af9976b0045a030f65
matthiasDev1989 <matthiasDev@gmx.at>
2024-10-25 08:01:14 +0200

    Initial commit.

---
+++ src/main/java/mn/devinfologbackend/DevInfoLogBackendApplication.java
@@ -0,0 +1,13 @@
+package mn.devinfologbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevInfoLogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevInfoLogBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mn/devinfologbackend/DevInfoLogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mn.devinfologbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevInfoLogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
